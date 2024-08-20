106905e24eca887cf9f1ea6745b13351a9f7c5a5
sonti123l <116090293+sonti123l@users.noreply.github.com>
2024-08-20 21:32:17 +0530

    Delete java-Start directory

--- java-Start/Sum.java
+++
@@ -1,9 +0,0 @@
-public class Sum {
-    public static void main(String[] args) {
-        byte MyByteValue = 127;
-        short MyShortValue = 32430;
-        int MyIntValue = 12345;
-        long MyLongValue = 50_000L+10L*(MyByteValue+MyIntValue+MyShortValue);
-        System.out.println(MyLongValue);
-    }
-}
